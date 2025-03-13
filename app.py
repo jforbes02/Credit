d7264df0055e6e0fa3d452da73df9f37adc21362
Justin <justinforbes559@gmail.com>
2025-03-13 17:24:39 -0400

    Registration route completed. Database tweaked to work, Learned how to handle foreign keys.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-from flask_login import LoginManager
+from flask import Flask, render_template, redirect, flash, url_for, request, session
+from flask_login import LoginManager, logout_user, login_required, current_user
 from livereload import Server
+from werkzeug.security import generate_password_hash
+
 from database import db, User, Config, Transaction
 
 """
@@ -14,21 +16,48 @@ App that
 """
 app = Flask(__name__)
 app.config.from_object(Config)
+app.secret_key="secreter"
 db.init_app(app)
-with app.app_context():
-    db.create_all()
+
 #class UserForm(Form):
 @app.route('/') #when they go to root of website
 def index():
-    return "drink"
+    return "drink "
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        age = request.form['age']
+        #one email per username
+        existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
+        if existing_user:
+            flash('Username already exists')
+            return redirect(url_for('register'))
+
+        hashed_pass = generate_password_hash(password)
+        new_user = User(username=username, email=email, password=hashed_pass, age=age)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful!')
+        return redirect(url_for('login'))
     return render_template("registration.html")
 
 @app.route('/login')
 def login():
     return render_template("login.html")
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out")
+    return redirect(url_for("login"))
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(80), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    credit_limit = db.Column(db.Integer, nullable=False)
+    credit_limit = db.Column(db.Integer, default=1000, nullable=False)
     current_balance = db.Column(db.Integer, default=0, nullable=False)
     account_status = db.Column(db.String(20), default='active')
     password = db.Column(db.String(80), nullable=False)
@@ -27,14 +27,12 @@ class User(db.Model, UserMixin):
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
     amount = db.Column(db.Integer, nullable=False)
     type = db.Column(db.String(20), nullable=False) #payement,cred^,purchase
     description = db.Column(db.String(255), nullable=False) #context
     created_at = db.Column(db.DateTime,nullable=False, default=datetime.utcnow)
-
-    user = db.relationship('User', backref='transactions', lazy=True) #allows access of all transactions from a user
-
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref='transaction', lazy='select')
     def __repr__(self):
         return f'<Transaction {self.id}: {self.transaction_type} ${self.amount}>'
 
